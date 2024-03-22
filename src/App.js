7698a3d08d8faed031d0ef151dd67976117d487a
katie <katie924p@gmail.com>
2024-03-22 18:15:59 +0800

    <練習>連結和Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetup'
+import FavoritesPage from './pages/Favorites'
+import MainNavigation from './components/layout/MainNavigation'
+
 function App() {
-  return <div>Hello!</div>;
+    return (
+        <>
+            <MainNavigation />
+            <Routes>
+                <Route path='/' element={<AllMeetupsPage />} />
+                <Route path='/new-meetup' element={<NewMeetupPage />} />
+                <Route path='/favorites' element={<FavoritesPage />} />
+            </Routes>
+        </>
+    )
 }
 
-export default App;
+export default App
